a8e87617f30b04714a402cba647f8f6563151813
shou yamamoto <yamasyo0017@gmail.com>
2020-12-22 07:10:57 +0900

    add quizを表示させる

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 {
+  const quiz = document.getElementById('quiz');
   const answer = document.getElementById('input_message');
   const input_btn = document.getElementById('input_btn');
 
@@ -8,9 +9,17 @@
     {q: 'appendの意味は？', a: '追加'},
     {q: 'parentの意味は？', a: '親'},
     {q: 'childの意味は？', a: '子'}
-  ]
+  ];
 
+  let currentNum = 0;
 
+  function setQuiz() {
+    quiz.textContent = quizList[currentNum].q;
+  }
+
+  setQuiz();
+
+  //btnを押すと答え合わせをする
   input_btn.addEventListener('click', () => {
   });
 }
