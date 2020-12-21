d7ec7de8147455435aad31aa93c25e584f1fc33d
shou yamamoto <yamasyo0017@gmail.com>
2020-12-22 08:31:22 +0900

    check answer

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
   const quiz = document.getElementById('quiz');
   const answer = document.getElementById('input_message');
   const input_btn = document.getElementById('input_btn');
+  const next_btn = document.getElementById('next_btn');
 
   const quizList = [
     {q: 'appendの意味は？', a: '追加'},
@@ -19,7 +20,39 @@
 
   setQuiz();
 
-  //btnを押すと答え合わせをする
+  //inputに答えを入力するとbtnの色が変化する
+  function btnColor() {
+    if(answer.value.length == 0) { //入力されていなければbtnの色は変更しない
+      input_btn.classList.remove('check_answer');
+    } else if(answer.value.length >= 1) {　//1文字でも入力されていればbtnの色を変更
+      input_btn.classList.add('check_answer');
+    }
+  }
+
+  function check_answer() {
+    if(answer.value === quizList[currentNum].a ) {
+      answer.classList.add('correct');
+    } else {
+      answer.classList.add('wrong');
+    }
+  }
+
+  answer.addEventListener('input', () => {
+    btnColor();
+  });
+
+  //input_btnを押すと答え合わせをする
   input_btn.addEventListener('click', () => {
+    if(input_btn.classList.contains('check_answer')) {
+      check_answer();
+    }
+  });
+  
+  // next_btnを押すと問題を進める
+  next_btn.addEventListener('click', () => {
+    currentNum ++; //問題を1つ進める
+    answer.value = ''; //inputの中を初期化する
+    input_btn.classList.remove('check_answer'); //btnのcheck_answerクラスを削除
+    setQuiz();
   });
 }
